41e592f7fb50db12fefbcb3fd4347b587a40357a
joãoA <joaovictor.dudu03@gmail.com>
2024-12-21 09:56:54 -0300

    deixando responsivo para o mobile

--- source/main.js
+++ source/main.js
@@ -1,6 +1,22 @@
 document.addEventListener('DOMContentLoaded', function(){
     const buttons = document.querySelectorAll('[data-tab-button]')
+    const questions = document.querySelectorAll('[data-faq-question]')
+    const heroSection = document.querySelector('.hero')
+
+    const alturaHero = heroSection.clientHeight
     
+    window.addEventListener('scroll', function(){
+        const posicaoAtual = window.scrollY
+
+        if(posicaoAtual < alturaHero){
+            ocultaElementosDoHeader()
+        }
+        else{
+            exibeElementosDoHeader()
+        }
+    })
+
+    //Seção de atrações
     for(let i = 0; i<buttons.length; i++){
         buttons[i].addEventListener('click', function(botao){
             const abaAlvo = botao.target.dataset.tabButton
@@ -11,7 +27,23 @@ document.addEventListener('DOMContentLoaded', function(){
             botao.target.classList.add('shows__tabs__button--is-active')
         })
     }
+
+    //seção faq
+    for(let i = 0; i<questions.length; i++){
+        questions[i].addEventListener('click', abreFechaFaq)
+    }
 })
+
+function ocultaElementosDoHeader(){
+    const header = document.querySelector('.header')
+    header.classList.add('header--is-hidden')
+}
+
+function exibeElementosDoHeader(){
+    const header = document.querySelector('.header')
+    header.classList.remove('header--is-hidden')
+}
+
 
 function removeBotaoAtivo(){
     const buttons = document.querySelectorAll('[data-tab-button]')
@@ -27,4 +59,10 @@ function escondeTodasAbas(){
     for(let i = 0; i < tabsContainer.length; i++){
         tabsContainer[i].classList.remove('shows__list--is-active')
     }
+}
+
+function abreFechaFaq(elemento){
+    // const classe = 'faq__questions__item--is-open'
+    const elementoPai = elemento.target.parentNode
+    elementoPai.classList.toggle('faq__questions__item--is-open')
 }
